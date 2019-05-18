e235c7da4a9f98bbb943adf10eb48b88000ca261
kavya.pl <kavya.pl@gmail.com>
2019-05-18 11:55:20 +0530

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,53 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { MenuComponent } from "./menu/menu.component";
+import { NavComponent } from "./nav/nav.component";
+import { HomeComponent } from "./home/home.component";
+import { ProductsComponent } from "./products/products.component";
+import { ServicesComponent } from "./services/services.component";
+import { AboutsComponent } from "./abouts/abouts.component";
 
+import { RouterModule } from "@angular/router";
+import { FallbackComponent } from "./fallback/fallback.component";
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    NavComponent,
+    HomeComponent,
+    ProductsComponent,
+    ServicesComponent,
+    AboutsComponent,
+    FallbackComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: "",
+        component: HomeComponent
+      },
+      {
+        path: "services",
+        component: ServicesComponent
+      },
+      {
+        path: "products",
+        component: ProductsComponent
+      },
+      {
+        path: "aboutus",
+        component: AboutsComponent
+      },
+      {
+        path: "**",
+        component: FallbackComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
